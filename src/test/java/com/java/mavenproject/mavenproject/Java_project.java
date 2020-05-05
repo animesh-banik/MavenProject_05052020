bad9c345a00dd5181b2cc15721a8f01379049d22
unknown <animeshbanik2018@gmail.com>
2020-05-05 20:24:56 +0530

    Master Update

---
+++ src/test/java/com/java/mavenproject/mavenproject/Java_project.java
@@ -0,0 +1,10 @@
+package com.java.mavenproject.mavenproject;
+
+public class Java_project {
+
+	public static void main(String[] args) {
+		 
+        System.out.println("Code added from master");
+	}
+
+}
