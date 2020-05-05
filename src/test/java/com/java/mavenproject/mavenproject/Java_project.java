360bf97d738e0f1cefd8515c491a73aeff573a06
unknown <animeshbanik2018@gmail.com>
2020-05-05 21:00:23 +0530

    branchUpdate

--- src/test/java/com/java/mavenproject/mavenproject/Java_project.java
+++ src/test/java/com/java/mavenproject/mavenproject/Java_project.java
@@ -5,6 +5,7 @@ public class Java_project {
 	public static void main(String[] args) {
 		 
         System.out.println("Code added from master");
+        System.out.println("Code added from Branch1");
 	}
 
 }
