2a1fdb0959831688f9b9dad0e17f047fbb4c30b2
unknown <animeshbanik2018@gmail.com>
2020-05-10 20:37:11 +0530

    1

---
+++ src/test/java/com/java/mavenproject/mavenproject/Dev1_funtionality.java
@@ -0,0 +1,11 @@
+package com.java.mavenproject.mavenproject;
+
+public class Dev1_funtionality {
+	
+	public  void getval_dev1()
+	{
+		
+			System.out.println("Update from from clone1 Branch1 for pull request.");
+	}
+
+}
--- src/test/java/com/java/mavenproject/mavenproject/Java_project.java
+++ src/test/java/com/java/mavenproject/mavenproject/Java_project.java
@@ -6,8 +6,12 @@ public class Java_project {
 		 
         System.out.println("Code added from master");
         System.out.println("Code added from Branch1");
+        System.out.println("Code added from Branch1");
         
+        UpdateFromClone1 abc = new UpdateFromClone2();
+        System.out.println("Code added from Branch1");
         UpdateFromClone1 abc = new UpdateFromClone1();
+
         abc.getval1();
         abc.getval2();
         abc.revert1();
@@ -18,7 +22,14 @@ public class Java_project {
         abcd.getval1();
         abcd.getval2();
 
-
+        UpdateFromClone2 test = new UpdateFromClone2();
+        test.getval1();
+        
+        Dev1_funtionality dev1= new Dev1_funtionality();
+        dev1.getval_dev1();
+        Dev1_funtionality2 dev2= new Dev1_funtionality2();
+        dev2.getval_dev2();
+        dev2.getval_dev3();
 	}
 
 }
